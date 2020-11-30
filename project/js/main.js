9ce10c477d2df5fb2cc849a24a56f984cc25a301
DigiGlee <digiglee@gmail.com>
2020-11-29 20:22:22 -0500

    Resume files

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,97 @@
+$(document).ready(function(){
+//   getWeather();
+     getPosts();
+ })
+
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+function addMessage(postTitle,postBody){
+  var postData ={
+    title: postTitle,
+    body: postBody
+  }
+
+var database = firebase.database().ref("posts");
+
+var newPostRef = database.push();
+newPostRef.set(postData, function(error) {
+  if (error) {
+    // The write failed...
+  } else {
+    // Data saved successfully!
+    window.location.reload();
+  }
+});
+}
+function handleMessageFormSubmit(){
+  var postTitle = $("#post-title").val();
+  var postBody = $("#post-body").val();
+  console.log(postTitle);
+  console.log(postBody);
+  addMessage(postTitle, postBody);
+
+}
+
+function getPosts(){
+  return firebase.database().ref("posts").once('value').then(function(snapshot) {
+    var posts = snapshot.val();
+    console.log(posts);
+
+    for(var postkey in posts){
+      var post = posts[postkey];
+      $("#post-listing").append("<div>"+post.title+" - "+post.body+"</div>");
+    }
+  });
+}
+
+
+function getWeather(searchQuery){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&APPID="+apikey;
+  
+  $(".error-message").text("");
+  $(".city").text("");
+  $(".temp").text("");
+
+  $.ajax(url, {success:function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error occurred");
+  }})
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
   $("p").html("High five! You're building your first web app!");
-
+ 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
 }
+
+$('.dropdown-toggle').dropdown()
+
+$("#banner").append('<img class="" src="images/banner.jpg"/>');
--- project/start.js
+++ project/start.js
@@ -1,21 +1,42 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'public')));
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
+app.get('*', function(request, response) {
+  response.render('index.html');
+});
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
 // This file is what handles incoming requests and
